6c04fb6b7ee60b92bcdd98a6d54dffd3f83bb1b3
Ryan Balla <ryanballa@Ryans-MacBook-Air.local>
2024-03-12 12:48:56 -0500

    GH pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = 'production' === 'development';
-
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs'
-		}),
+		adapter: adapter(),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/cosmicbowlinggraph' : ''
 		}
